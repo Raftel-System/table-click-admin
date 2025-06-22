09b358ce6f7ab7c1edab937d565e598b92ccc4ff
= <=>
2025-06-22 21:51:02 +0200

    add https

--- src/hooks/useOrders.ts
+++ src/hooks/useOrders.ts
@@ -317,11 +317,11 @@ export const useOrders = (restaurantSlug: string) => {
 
             console.log('🖨️ Envoi vers imprimante:', {
                 ...printData,
-                serverUrl: `http://${config.serverPrinterIp}:3001/print-ticket`
+                serverUrl: `https://zeus-lab.tailfdaef5.ts.net/print-ticket` //http://${config.serverPrinterIp}:3001/print-ticket
             });
 
             // 4. Envoyer la requête au serveur d'impression
-            const response = await fetch(`http://${config.serverPrinterIp}:3001/print-ticket`, {
+            const response = await fetch(`https://zeus-lab.tailfdaef5.ts.net/print-ticket`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
