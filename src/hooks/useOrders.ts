eeb9fb9dd9971e168dcef15cb91ae2c7fee0f97a
= <=>
2025-07-06 15:36:28 +0200

    fixes

--- src/hooks/useOrders.ts
+++ src/hooks/useOrders.ts
@@ -662,7 +662,7 @@ export const useOrders = (restaurantSlug: string) => {
             };
 
             // 4. Envoyer la requête au serveur d'impression
-            const response = await fetch(`http://192.168.1.64:3001/print-ticket-log`, {
+            const response = await fetch(`http://192.168.1.64:3001/print-ticket`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
